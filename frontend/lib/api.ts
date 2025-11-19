5f0bf0ab21a3ff24fca9bceb2dcc32bb43a194cc
Kamol <63007078+kmlo8@users.noreply.github.com>
2025-11-19 12:51:41 +0900

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+// whi is this does not work
 const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001';
 
   getLikedDirectors: () => api.get('/api/user/liked-directors'),
